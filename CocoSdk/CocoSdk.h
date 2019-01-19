f8b4dd6e07631cdee344c1ae735dae946a05f5be
Iman Kazemeini <i.kazemeini@Users-iMac.local>
2019-01-19 15:06:58 +0330

    init

---
+++ CocoSdk/CocoSdk.h
@@ -0,0 +1,19 @@
+//
+//  CocoSdk.h
+//  CocoSdk
+//
+//  Created by Iman Kazemeini on 1/19/19.
+//  Copyright © 2019 Iman Kazemeini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoSdk.
+FOUNDATION_EXPORT double CocoSdkVersionNumber;
+
+//! Project version string for CocoSdk.
+FOUNDATION_EXPORT const unsigned char CocoSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoSdk/PublicHeader.h>
+
+
---
+++ CocoSdk/SdkHelper.swift
@@ -0,0 +1,21 @@
+//
+//  SdkHelper.swift
+//  CocoSdk
+//
+//  Created by Iman Kazemeini on 1/19/19.
+//  Copyright © 2019 Iman Kazemeini. All rights reserved.
+//
+
+import Foundation
+
+
+class SdkHelper {
+    
+    func add(int1:Int,int2:Int) -> Int {
+        return (int1+int2)
+    }
+    
+    
+    
+    
+}
